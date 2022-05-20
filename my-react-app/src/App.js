f71ba7caa2770165d68ad08fc650cb5aa660c252
pardue95 <85760640+pardue95@users.noreply.github.com>
2022-05-20 11:29:39 -0500

    added preventDefault and inital styling

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="">Contact Import</h1>
+      <div
+        style={{
+          border: "2px solid red",
+        }}
+        className={"border: '1px solid red'"}
+        onDragOver={(e) => {
+          e.preventDefault();
+        }}
+        onDrop={(e) => {
+          e.preventDefault();
+          console.log(e);
+        }}
+      >
+        Drop Here
+      </div>
     </div>
   );
 }
