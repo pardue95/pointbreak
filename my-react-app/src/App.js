a6d8067b18957bce1d7d2be255e9b44c8c0bc806
pardue95 <85760640+pardue95@users.noreply.github.com>
2022-06-01 12:37:47 -0500

    working

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react"
+import React from "react";
 
 function App() {
+  const [highlighted, setHighlighted] = React.useState(false);
+
   return (
     <div className="App">
       <h1 className="">Contact Import</h1>
       <div
-        style={{
-          border: "2px solid red",
+        // style={{
+        //   border: "2px solid red",
+        // }}
+        className={'${highlighted}'}
+        onDragEnter={() => {
+          setHighlighted(true);
         }}
-        className={"border: '1px solid red'"}
+        onDragLeave={() => {
+          setHighlighted(false);
+        }}
         onDragOver={(e) => {
           e.preventDefault();
         }}
         onDrop={(e) => {
           e.preventDefault();
-          console.log(e);
+          setHighlighted(false);
+
+          Array.from(e.dataTransfer.files)
+            .filter((file) => file.type === "text/csv")
+            .forEach((file) => {
+              console.log(file);
+            });
         }}
       >
         Drop Here
